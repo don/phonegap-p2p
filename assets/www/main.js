692157a2cf3f33e535954d0a1a7f5ce0827f0959
Don Coleman <dcoleman@chariotsolutions.com>
2013-01-11 17:48:26 -0500

    old css and js

--- assets/www/main.js
+++ assets/www/main.js
@@ -1,165 +1,97 @@
-/*
-       Licensed to the Apache Software Foundation (ASF) under one
-       or more contributor license agreements.  See the NOTICE file
-       distributed with this work for additional information
-       regarding copyright ownership.  The ASF licenses this file
-       to you under the Apache License, Version 2.0 (the
-       "License"); you may not use this file except in compliance
-       with the License.  You may obtain a copy of the License at
+/*global Ndef */
 
-         http://www.apache.org/licenses/LICENSE-2.0
-
-       Unless required by applicable law or agreed to in writing,
-       software distributed under the License is distributed on an
-       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-       KIND, either express or implied.  See the License for the
-       specific language governing permissions and limitations
-       under the License.
-*/
-
-var deviceInfo = function() {
-    document.getElementById("platform").innerHTML = device.platform;
-    document.getElementById("version").innerHTML = device.version;
-    document.getElementById("uuid").innerHTML = device.uuid;
-    document.getElementById("name").innerHTML = device.name;
-    document.getElementById("width").innerHTML = screen.width;
-    document.getElementById("height").innerHTML = screen.height;
-    document.getElementById("colorDepth").innerHTML = screen.colorDepth;
-};
-
-var getLocation = function() {
-    var suc = function(p) {
-        alert(p.coords.latitude + " " + p.coords.longitude);
-    };
-    var locFail = function() {
-    };
-    navigator.geolocation.getCurrentPosition(suc, locFail);
-};
-
-var beep = function() {
-    navigator.notification.beep(2);
-};
-
-var vibrate = function() {
-    navigator.notification.vibrate(0);
-};
-
-function roundNumber(num) {
-    var dec = 3;
-    var result = Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
-    return result;
-}
-
-var accelerationWatch = null;
-
-function updateAcceleration(a) {
-    document.getElementById('x').innerHTML = roundNumber(a.x);
-    document.getElementById('y').innerHTML = roundNumber(a.y);
-    document.getElementById('z').innerHTML = roundNumber(a.z);
-}
-
-var toggleAccel = function() {
-    if (accelerationWatch !== null) {
-        navigator.accelerometer.clearWatch(accelerationWatch);
-        updateAcceleration({
-            x : "",
-            y : "",
-            z : ""
+function unshareTag() {
+    nfc.unshare(
+        function () {
+            navigator.notification.vibrate(100);
+            setTimeout(function() {
+                navigator.notification.vibrate(100);
+            }, 200);
+        }, function () {
+            alert("Failed to unshare tag.");
         });
-        accelerationWatch = null;
+}
+
+function shareTag() {
+    var mimeType = document.forms[0].elements["mimeType"].value,
+        payload = document.forms[0].elements["payload"].value,
+        record = ndef.mimeMediaRecord(mimeType, nfc.stringToBytes(payload));
+
+    nfc.share(
+        [record],
+        function () {
+            navigator.notification.vibrate(100);
+        }, function () {
+            alert("Failed to share tag.");
+        });
+}
+
+function onChange(e) {
+    if (e.target.checked) {
+        shareTag();
     } else {
-        var options = {};
-        options.frequency = 1000;
-        accelerationWatch = navigator.accelerometer.watchAcceleration(
-                updateAcceleration, function(ex) {
-                    alert("accel fail (" + ex.name + ": " + ex.message + ")");
-                }, options);
-    }
-};
-
-var preventBehavior = function(e) {
-    e.preventDefault();
-};
-
-function dump_pic(data) {
-    var viewport = document.getElementById('viewport');
-    console.log(data);
-    viewport.style.display = "";
-    viewport.style.position = "absolute";
-    viewport.style.top = "10px";
-    viewport.style.left = "10px";
-    document.getElementById("test_img").src = data;
-}
-
-function fail(msg) {
-    alert(msg);
-}
-
-function show_pic() {
-    navigator.camera.getPicture(dump_pic, fail, {
-        quality : 50
-    });
-}
-
-function close() {
-    var viewport = document.getElementById('viewport');
-    viewport.style.position = "relative";
-    viewport.style.display = "none";
-}
-
-function contacts_success(contacts) {
-    alert(contacts.length
-            + ' contacts returned.'
-            + (contacts[2] && contacts[2].name ? (' Third contact is ' + contacts[2].name.formatted)
-                    : ''));
-}
-
-function get_contacts() {
-    var obj = new ContactFindOptions();
-    obj.filter = "";
-    obj.multiple = true;
-    navigator.contacts.find(
-            [ "displayName", "name" ], contacts_success,
-            fail, obj);
-}
-
-function check_network() {
-    var networkState = navigator.network.connection.type;
-
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.NONE]     = 'No network connection';
-
-    confirm('Connection type:\n ' + states[networkState]);
-}
-
-var watchID = null;
-
-function updateHeading(h) {
-    document.getElementById('h').innerHTML = h.magneticHeading;
-}
-
-function toggleCompass() {
-    if (watchID !== null) {
-        navigator.compass.clearWatch(watchID);
-        watchID = null;
-        updateHeading({ magneticHeading : "Off"});
-    } else {        
-        var options = { frequency: 1000 };
-        watchID = navigator.compass.watchHeading(updateHeading, function(e) {
-            alert('Compass Error: ' + e.code);
-        }, options);
+        unshareTag();
     }
 }
 
-function init() {
-    // the next line makes it impossible to see Contacts on the HTC Evo since it
-    // doesn't have a scroll button
-    // document.addEventListener("touchmove", preventBehavior, false);
-    document.addEventListener("deviceready", deviceInfo, true);
+var ready = function () {
+    document.getElementById('checkbox').addEventListener("change", onChange, false);
+};
+
+document.addEventListener('deviceready', ready, false);
+
+var data = [
+    {
+        mimeType: 'text/x-vCard',
+        payload: 'BEGIN:VCARD\n' +
+            'VERSION:2.1\n' +
+            'N:Coleman;Don;;;\n' +
+            'FN:Don Coleman\n' +
+            'ORG:Chariot Solutions;\n' +
+            'URL:http://chariotsolutions.com\n' +
+            'TEL;WORK:215-358-1780\n' +
+            'EMAIL;WORK:dcoleman@chariotsolutions.com\n' +
+            'END:VCARD'
+    },
+    {
+        mimeType: 'text/x-vCard',
+        payload: 'BEGIN:VCARD\n' +
+            'VERSION:2.1\n' +
+            'N:Griffin;Kevin;;;\n' +
+            'FN:Kevin Griffin\n' +
+            'ORG:Chariot Solutions;\n' +
+            'URL:http://chariotsolutions.com\n' +
+            'TEL;WORK:215-358-1780\n' +
+            'EMAIL;WORK:kgriffin@chariotsolutions.com\n' +
+            'END:VCARD'
+    },
+    {
+        mimeType: 'game/rockpaperscissors',
+        payload: 'Rock'
+    },
+    {
+        mimeType: 'text/pg',
+        payload: 'Hello PhoneGap'
+    },
+    {
+        mimeType: '',
+        payload: ''
+    }
+];
+
+var index = 0;
+function showSampleData() {
+    var mimeTypeField = document.forms[0].elements["mimeType"],
+      payloadField = document.forms[0].elements["payload"],
+      record = data[index];
+    
+    index++;
+    if (index >= data.length) {
+        index = 0;
+    }
+    
+    mimeTypeField.value = record.mimeType;
+    payloadField.value = record.payload;    
 }
+
+document.addEventListener("menubutton", showSampleData, false);
