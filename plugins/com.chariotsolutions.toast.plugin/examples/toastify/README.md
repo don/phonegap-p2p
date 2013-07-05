3db919353199c7947571305652030d973d294edc
Don Coleman <dcoleman@chariotsolutions.com>
2013-07-05 17:17:19 -0400

    cordova plugin add https://github.com/don/Toasty.git

---
+++ plugins/com.chariotsolutions.toast.plugin/examples/toastify/README.md
@@ -0,0 +1,6 @@
+This example is built with [cordova-cli](https://npmjs.org/package/cordova).
+
+Build, install and run example from this directory.
+    
+    $ cordova run
+    
---
+++ plugins/com.chariotsolutions.toast.plugin/examples/toastify/plugins/com.chariotsolutions.toast.plugin/www/phonegap-toast.js
@@ -0,0 +1,14 @@
+/*global cordova, module*/
+module.exports = {
+    showShort: function (message, win, fail) {
+        cordova.exec(win, fail, "Toasty", "show_short", [message]);
+    },
+
+    showLong: function (message, win, fail) {
+        cordova.exec(win, fail, "Toasty", "show_long", [message]);
+    },
+
+    cancel: function (win, fail) {
+        cordova.exec(win, fail, "Toasty", "cancel", []);
+    }
+};
