9ccaefe226513e1f3907db93591faff692f26743
Don Coleman <dcoleman@chariotsolutions.com>
2013-11-07 17:30:55 -0500

    re-install plugins

--- platforms/wp8/www/cordova_plugins.js
+++ platforms/wp8/www/cordova_plugins.js
@@ -1,3 +1,16 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = []
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/com.chariotsolutions.nfc.plugin/www/phonegap-nfc.js",
+        "id": "com.chariotsolutions.nfc.plugin.NFC",
+        "runs": true
+    }
+]
 });
