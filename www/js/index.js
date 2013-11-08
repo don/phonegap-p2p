076f2de8d1f583698386d81b1f66d597139bc088
Don Coleman <dcoleman@chariotsolutions.com>
2013-11-07 19:42:17 -0500

    update javascript to newer style

--- www/js/index.js
+++ www/js/index.js
@@ -1,91 +1,113 @@
 /*jshint quotmark: false */
-/*global nfc, ndef, toast, alert, cordova, checkbox, statusDiv */
+/*global nfc, ndef, toast, alert, cordova, checkbox, statusDiv, sample */
 
 "use strict";
 
-var android = (cordova.platformId === 'android');
-var windowsphone = (cordova.platformId === 'windowsphone');
-var bb10 = (cordova.platformId === 'blackberry10');
+var android = (cordova.platformId === 'android'),
+    windowsphone = (cordova.platformId === 'windowsphone'),
+    bb10 = (cordova.platformId === 'blackberry10'),
+    sampleData;
 
-function disableUI() {
-    document.forms[0].elements.mimeType.disabled = true;
-    document.forms[0].elements.payload.disabled = true;
-}
+var app = {
+    sampleDataIndex: 0,
+    initialize: function () {
+        this.bind();
+    },
+    bind: function () {
+        document.addEventListener('deviceready', app.deviceready, false);
+    },
+    deviceready: function () {
+        checkbox.addEventListener('change', app.toggleCheckbox, false);
+        sample.addEventListener('click', app.showSampleData, false);
+    },
+    disableUI: function () {
+        document.forms[0].elements.mimeType.disabled = true;
+        document.forms[0].elements.payload.disabled = true;
+    },
+    enableUI: function () {
+        document.forms[0].elements.mimeType.disabled = false;
+        document.forms[0].elements.payload.disabled = false;
+    },
+    shareMessage: function () {
+        var mimeType = document.forms[0].elements.mimeType.value,
+            payload = document.forms[0].elements.payload.value,
+            record = ndef.mimeMediaRecord(mimeType, nfc.stringToBytes(payload));
 
-function enableUI() {
-    document.forms[0].elements.mimeType.disabled = false;
-    document.forms[0].elements.payload.disabled = false;
-}
+        app.disableUI();
 
-function notifyUser(message) {
-    if (android) {
-        toast.showShort(message);
-    } else {
-        statusDiv.innerHTML = message;
-        setTimeout(function() {
-            statusDiv.innerHTML = "";
-        }, 3000);
-    }
-}
-
-function unshareMessage() {
-
-    enableUI();
-
-    nfc.unshare(
-        function () {
-            navigator.notification.vibrate(100);
-            notifyUser("Message is no longer shared.");
-        }, function (reason) {
-            alert("Failed to unshare message " + reason);
-        });
-}
-
-function shareMessage() {
-    var mimeType = document.forms[0].elements.mimeType.value,
-        payload = document.forms[0].elements.payload.value,
-        record = ndef.mimeMediaRecord(mimeType, nfc.stringToBytes(payload));
-
-    disableUI();
-
-    nfc.share(
-        [record],
-        function () {
-            if (bb10) {
-                // Blackberry calls success as soon as the Card appears
+        nfc.share(
+            [record],
+            function () {
+                if (bb10) {
+                    // Blackberry calls success as soon as the Card appears
+                    checkbox.checked = false;
+                    app.enableUI();
+                } else if (windowsphone) {
+                    // Windows phone calls success immediately. Bug?
+                    app.notifyUser("Sharing Message");
+                } else {
+                    // Android call the success callback when the message is sent to peer
+                    navigator.notification.vibrate(100);
+                    app.notifyUser("Sent Message to Peer");
+                }
+            }, function (reason) {
+                alert("Failed to share tag " + reason);
                 checkbox.checked = false;
-                enableUI();
-            } else if (windowsphone) {
-                // Windows phone calls success immediately. Bug?
-                notifyUser("Sharing Message");
-            } else {
-                // Android call the success callback when the message is sent to peer
-                navigator.notification.vibrate(100);
-                notifyUser("Sent Message to Peer");
+                app.enableUI();
             }
-        }, function (reason) {
-            alert("Failed to share tag " + reason);
-            checkbox.checked = false;
-            enableUI();
-        });
-}
+        );
+    },
+    unshareMessage: function () {
+        app.enableUI();
 
-function onChange(e) {
-    if (e.target.checked) {
-        shareMessage();
-    } else {
-        unshareMessage();
+        nfc.unshare(
+            function () {
+                navigator.notification.vibrate(100);
+                app.notifyUser("Message is no longer shared.");
+            }, function (reason) {
+                alert("Failed to unshare message " + reason);
+            }
+        );
+    },
+    notifyUser: function (message) {
+        if (android) {
+            toast.showShort(message);
+        } else {
+            statusDiv.innerHTML = message;
+            setTimeout(function() {
+                statusDiv.innerHTML = "";
+            }, 3000);
+        }
+    },
+    showSampleData: function() {
+        var mimeTypeField = document.forms[0].elements.mimeType,
+          payloadField = document.forms[0].elements.payload,
+          record = sampleData[app.sampleDataIndex];
+
+        if (mimeTypeField.disabled) {
+            app.notifyUser("Unshare Message to edit data");
+            return false;
+        }
+
+        app.sampleDataIndex++;
+        if (app.sampleDataIndex >= sampleData.length) {
+            app.sampleDataIndex = 0;
+        }
+
+        mimeTypeField.value = record.mimeType;
+        payloadField.value = record.payload;
+        return false;
+    },
+    toggleCheckbox: function (e) {
+        if (e.target.checked) {
+            app.shareMessage();
+        } else {
+            app.unshareMessage();
+        }
     }
-}
-
-var ready = function () {
-    document.getElementById('checkbox').addEventListener("change", onChange, false);
-    document.getElementById('sample').addEventListener("click", showSampleData, false);
 };
 
-document.addEventListener('deviceready', ready, false);
-
-var data = [
+sampleData = [
     {
         mimeType: 'text/pg',
         payload: 'Hello PhoneGap'
@@ -111,24 +133,3 @@ var data = [
         payload: ''
     }
 ];
-
-var index = 0;
-function showSampleData() {
-    var mimeTypeField = document.forms[0].elements.mimeType,
-      payloadField = document.forms[0].elements.payload,
-      record = data[index];
-
-    if (mimeTypeField.disabled) {
-        notifyUser("Unshare Message to edit data");
-        return false;
-    }
-
-    index++;
-    if (index >= data.length) {
-        index = 0;
-    }
-
-    mimeTypeField.value = record.mimeType;
-    payloadField.value = record.payload;
-    return false;
-}
